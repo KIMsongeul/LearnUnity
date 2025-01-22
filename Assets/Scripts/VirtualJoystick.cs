6480c09d6227c6ebab895826e4d3ab346f35c04e
songeul <0754minjun@gmail.com>
2025-01-22 11:24:10 +0900

    IBegin,IDrag,IEndHandler코드에 주석달기

--- Assets/Scripts/VirtualJoystick.cs
+++ Assets/Scripts/VirtualJoystick.cs
@@ -7,16 +7,19 @@ public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 //드래그를 사용하기 위해 각각 기능을 구현할 인터페이스를 추가해야 한다.
 {
     public void OnBeginDrag(PointerEventData eventData)
+    //이 스크립트가 붙은 오브젝트에다가 마우스 드래그를 시작했을 때 호출
     {
         Debug.Log("드래그를 시작합니다.");
     }
 
     public void OnDrag(PointerEventData eventData)
+    //이 스크립트가 붙은 오브젝트에다가 마우스 드래그 중인 동안 계속 호출
     {
         Debug.Log("핸들이 범위를 벗어나지 않게 제어해줍니다.");
     }
 
     public void OnEndDrag(PointerEventData eventData)
+    //이 스크립트가 붙은 오브젝트를 마우스 드래그 하는 것을 끝냈을 때 호출
     {
         Debug.Log("핸들을 초기화합니다.");
     }
