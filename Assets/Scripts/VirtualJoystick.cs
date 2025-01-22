589800f5fe41abd014148e8c21320ad13154d824
songeul <0754minjun@gmail.com>
2025-01-22 16:30:42 +0900

    조이스틱으로 이동구현

--- Assets/Scripts/VirtualJoystick.cs
+++ Assets/Scripts/VirtualJoystick.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IEndDragHandler
 //드래그를 사용하기 위해 각각 기능을 구현할 인터페이스를 추가해야 한다.
 {
-    public void OnBeginDrag(PointerEventData eventData)
-    //이 스크립트가 붙은 오브젝트에다가 마우스 드래그를 시작했을 때 호출
-    {
-        Debug.Log("드래그를 시작합니다.");
-    }
+    private float maxMagnitude = 100f;
+
+    public RectTransform handle;
+    //RectTransform은 보통 UI작업에 사용되는데 Transform과 다른점은 앵커(Anchor)라는 기준점을 가진다.
 
     public void OnDrag(PointerEventData eventData)
-    //이 스크립트가 붙은 오브젝트에다가 마우스 드래그 중인 동안 계속 호출
     {
-        Debug.Log("핸들이 범위를 벗어나지 않게 제어해줍니다.");
+        handle.anchoredPosition += eventData.delta;
+        //1. 드래그 입력이 들어온 만큼 움직인다.
+
+        Vector2 currentPosition = handle.anchoredPosition;
+        float currentMagnitude =
+            Mathf.Sqrt(currentPosition.x * currentPosition.x + currentPosition.y * currentPosition.y);
+        float currentAngleRadian = Mathf.Atan2(currentPosition.y, currentPosition.x);
+        //2. 범위를 넘어간 경우, 최대 범위 내에서 움직인다.
+
+        float clampedMagnitude = Mathf.Min(currentMagnitude, maxMagnitude);
+        var clampedPosition = new Vector2(clampedMagnitude * Mathf.Cos(currentAngleRadian),
+            clampedMagnitude * Mathf.Sin(currentAngleRadian));
+        //최대길이의 벡터 생성
+
+        handle.anchoredPosition = clampedPosition;
+        //위치를 조절된 벡터로 설정
     }
 
     public void OnEndDrag(PointerEventData eventData)
-    //이 스크립트가 붙은 오브젝트를 마우스 드래그 하는 것을 끝냈을 때 호출
     {
-        Debug.Log("핸들을 초기화합니다.");
+        handle.anchoredPosition = Vector3.zero;
     }
 }
